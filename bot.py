96112784236f6c9cd8a193e84f1582ac7b4d50c3
beerpiss <lacvtg.a1.2023@gmail.com>
2023-05-30 13:31:17 +0700

    init

---
+++ bot.py
@@ -0,0 +1,29 @@
+import random
+import re
+
+import discord
+from discord.ext.commands import Bot
+from dotenv import dotenv_values
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+cfg = dotenv_values()
+bot = Bot(command_prefix="!", intents=intents)
+
+very_cheap = re.compile(r"có(?: mỗi)? ([1-9]\d{1,2})k")
+funny = ["rất rẻ", "quá ít", "rất ít"]
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author.id != int(cfg["LUNA_USER_ID"]):
+        return
+    
+    m = very_cheap.search(message.content)
+    if m is None:
+        return
+    
+    price = m.group(1)
+    await message.reply(f"có {price}k ({random.choice(funny)})", mention_author=False)
+
+bot.run(cfg["TOKEN"])
